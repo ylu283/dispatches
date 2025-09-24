9b3b64b0adde4dae964859082c111f448a633fd3
ylu283 <139079568+ylu283@users.noreply.github.com>
2025-09-24 12:38:28 -0400

    update file location

--- dispatches/improved_pt/prescient_sweep/ERCOT_Base_case/base_pcm_ERCOT.py
+++ dispatches/improved_pt/prescient_sweep/ERCOT_Base_case/base_pcm_ERCOT.py
@@ -2,7 +2,7 @@ import os
 from dispatches.improved_pt.prescient_sweep.base_prescient_options import prescient_options
 from prescient.simulator import Prescient
 
-ercot_path = os.path.join(os.getcwd(), "..", "..", "..", "..", "..", "idaes-gtep/gtep/data/123_Bus_Coal/Prescient")
+ercot_path = os.path.join(os.getcwd(), "..", "..", "..", "..", "..", "idaes-gtep/gtep/data/123_Bus_Coal")
 
 prescient_options["output_directory"] = f"base_pcm_ERCOT"
 prescient_options["data_path"] = ercot_path
